0522eacaa0edeca59322a99b9b24319c89faad00
pingziizhengyi <yimsimix@outlook.com>
2024-10-21 23:00:33 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -7,7 +7,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGI5NzI0MDgwZTU1ZTcwMzcwZmI3NDI4NzkyMmYzMWIiLCJ0IjoiOGNlY2VlYzQtYzZiNi00N2VkLThhZjItY2I4MThmMDkxZWJkIiwicyI6Ik5XWTFNV1ZsWm1NdFpEYzJZeTAwWkdSaExUbGtZall0TnpneVpqZ3haVE00WkRBNSJ9';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNmZjNTU1NzMzNDUxOWFkMTQ1YTczOGYwNDgyMGU3N2QiLCJ0IjoiNmIxNDkzYmYtMDRhZi00NTRmLThhMDYtYmUyMzA2ZWE5NjYyIiwicyI6Ik1qY3pOek0zT1dRdE16QXhNaTAwWkRZekxUbGpPVFF0TmpSaU9UQXpObVpoWkRBeiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
